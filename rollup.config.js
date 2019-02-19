2a3aeb13650da0cbf7c29dd694e958c35b841ccf
Joseph Kohlmann <kohlmannj@mac.com>
2019-02-19 09:41:58 -0500

    Add a hasty first pass at IE 11 support

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,57 @@
-import typescript from 'rollup-plugin-typescript';
+// import typescript from 'rollup-plugin-typescript';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import pkg from './package.json';
 
-export default [
-	{
-		input: 'src/index.ts',
-		output: [
-			{ file: pkg.main, format: 'iife' }
-		],
-		name: '__shimport__',
-		plugins: [
-			replace({ __VERSION__: pkg.version }),
-			typescript({
-				typescript: require('typescript')
-			}),
-			terser(),
-			{
-				transformBundle(code) {
-					return {
-						code: code.replace('alert', '(0,eval)'),
-						map: null
-					};
-				}
-			},
-			filesize()
-		]
-	},
+const extensions = ['.js', '.ts'];
 
-	{
-		input: 'src/index.ts',
-		output: [
-			{ file: 'index.dev.js', format: 'iife' }
-		],
-		name: '__shimport__',
-		plugins: [
-			replace({ __VERSION__: pkg.version }),
-			typescript({
-				typescript: require('typescript')
-			}),
-			{
-				transformBundle(code) {
-					return {
-						code: code.replace('alert', '(0,eval)'),
-						map: null
-					};
-				}
-			}
-		]
-	}
+const babelPluginConfig = {
+  extensions,
+  exclude: 'node_modules/**',
+};
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [{ name: '__shimport__', file: pkg.main, format: 'iife' }],
+    plugins: [
+			nodeResolve({ extensions }),
+			commonjs(),
+      replace({ __VERSION__: pkg.version }),
+      babel(babelPluginConfig),
+      terser(),
+      {
+        renderChunk(code) {
+          return {
+            code: code.replace('alert', '(0,eval)'),
+            map: null,
+          };
+        },
+      },
+      filesize(),
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    output: [{ name: '__shimport__', file: 'index.dev.js', format: 'iife' }],
+    plugins: [
+			nodeResolve({ extensions }),
+			commonjs(),
+      replace({ __VERSION__: pkg.version }),
+      babel(babelPluginConfig),
+      {
+        renderChunk(code) {
+          return {
+            code: code.replace('alert', '(0,eval)'),
+            map: null,
+          };
+        },
+      },
+    ],
+  },
 ];
